0e082a1facb969c22600954a845baacdc8242828
Dan <d.c.budris@gmail.com>
2018-02-25 15:05:41 -0500

    reduced output numbers

--- src/main/java/edu/bu/cs755/Task2_2.java
+++ src/main/java/edu/bu/cs755/Task2_2.java
@@ -10,12 +10,10 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Map;
 import java.util.HashMap;
-
 import java.io.IOException;
 
 public class Task2_2 {
@@ -33,14 +31,6 @@ public class Task2_2 {
         ) throws IOException, InterruptedException {
             String line = value.toString();
             String[] fields = line.split("\t");
-
-            /* Debugging
-            System.out.println("\n---- STARTING MAP ---- \n");
-            System.out.println("--- FIELDS INPUT ---");
-            System.out.println(fields[0]+ "\n");
-            System.out.print(fields[1] + "\n");
-            */
-
             Text medallion = new Text(fields[0]);
             DoubleWritable percentage = new DoubleWritable(Double.parseDouble(fields[1]));
 
@@ -49,40 +39,31 @@ public class Task2_2 {
             map.put(medallion, percentage);
             q.addAll(map.entrySet());
 
-            // If the q is greater than 50, remove the least
+            // If the q is greater than 5, remove the least
             if (q.size() >= 6) {
-                /*
-                System.out.println("------- COMPARING IN Q --------");
-                System.out.println("Peek: " + q.peek().getValue() + " " + q.peek().getKey() + "\n % Value:" + percentage + "\n");
-                */
                 q.poll();
             }
         }
 
         public void cleanup(Context context
         ) throws IOException, InterruptedException {
-            System.out.println("----- Q SIZE ---- \n" + q.size());
             while(!q.isEmpty()) {
-
-                System.out.println("----- Q SIZE ---- \n" + q.size());
-                System.out.println("CLEANUP \n --PEEKING AT Q---");
-                System.out.println(q.peek());
-
                 context.write(q.peek().getValue(), q.peek().getKey());
                 q.remove();
             }
         }
     }
 
-    public static class MedallionErrorRateReducer extends Reducer<DoubleWritable,Text,Text,DoubleWritable> {
+    public static class MedallionErrorRateReducer extends Reducer<DoubleWritable,Text,DoubleWritable, Text> {
         public void reduce(DoubleWritable key, Iterable<Text> values,
                            Context context
         ) throws IOException, InterruptedException {
             for (Text val : values) {
-                context.write(val, key);
+                context.write(key, val);
             }
         }
     }
+
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         Job job =  new Job(conf, "task2.1");
@@ -91,8 +72,8 @@ public class Task2_2 {
         job.setMapOutputValueClass(Text.class);
         job.setMapperClass(Task2_2.SortMedallionErrors.class);
         job.setReducerClass(Task2_2.MedallionErrorRateReducer.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(DoubleWritable.class);
+        job.setOutputKeyClass(DoubleWritable.class);
+        job.setOutputValueClass(Text.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
